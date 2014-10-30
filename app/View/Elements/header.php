84cfe005ede841c189a902905cf6a14122ba2605
hupla78 <pierrebeauval78@hotmail.fr>
2014-10-30 13:14:27 +0100

    modif

---
+++ app/View/Elements/header.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fil de Bohème</title>
+    <link rel="stylesheet" href="../../webroot/css/foundation.css" />
+    <link rel="stylesheet" href="../../webroot/css/style.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  
+  <body>
+  
+    <div class="row">
+  <div class="off-canvas-wrap" data-offcanvas>
+  <div class="inner-wrap">
+    <nav class="tab-bar">
+      <section class="left-medium">
+        <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+      </section>
+
+      <section class="middle tab-bar-section">
+        <h1 class="title">Fil de Bohème</h1>
+      </section>
+
+  
+    </nav>
+
+    <aside class="left-off-canvas-menu">
+  <ul class="off-canvas-list">
+    <li><label>Menu</label></li>
+    <li><a href="#">Se connecter</a></li>
+    <li><a href="#">Panier</a></li>
+    <li><a href="#">Les nouveautés</a></li>
+  </ul>
+</aside>
+
+
+
+    <section class="main-section">
+    
+       <div class="large-5 columns medium-5 columns small-12 columns">
+      <img id="img1" src="img/logo2.jpg">
+      </div>
+      
+    <div class="large-4 columns medium-4 columns small-12 columns">
+      <div class="barre">
+      <form method="post" action="resultat.php" >
+      
+    <input  type="text" name="mot" />
+    <input  type="submit" name="form" value="Rechercher"/>
+    
+      </form>
+      </div>
+      
+   </div>
+      </section>   
+
+    
+    
+    </div>
+    </div>
+    
+    
+    
+  
+        
+    
+    <script src="js/foundation/foundation.js"></script>
+    <script src="js/foundation/foundation.orbit.js"></script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+      </body>
+    </html>
+        
